93d7aa4f053b83c257984d0ec37813e00ebe26b6
jolethen <155651557+jolethen@users.noreply.github.com>
2025-09-24 14:05:02 +0530

    Update reports.lua

--- reports.lua
+++ reports.lua
@@ -1,41 +1,17 @@
--- =====================
--- REPORT SYSTEM
--- =====================
+-- report.lua
 
-local reports = {}
-local report_id = 0
+-- Set your teleport location here
+local REPORT_COORDS = {x = 100, y = 10, z = 100}
 
-minetest.register_privilege("redit", {
-    description = "Can comment on reports",
-    give_to_admin = true
-})
-
--- /report <message>
 minetest.register_chatcommand("report", {
-    params = "<message>",
-    description = "File a report",
-    func = function(name, param)
-        if param == "" then
-            return false, "Usage: /report <message>"
+    description = "Teleport to the report center",
+    privs = {}, -- everyone can use
+    func = function(name)
+        local player = minetest.get_player_by_name(name)
+        if player then
+            player:set_pos(REPORT_COORDS)
+            return true, "You have been teleported to the report center!"
         end
-        report_id = report_id + 1
-        reports[report_id] = { owner = name, message = param, comments = {} }
-        return true, "Report #" .. report_id .. " submitted."
-    end
-})
-
--- /comment <id> <message>
-minetest.register_chatcommand("comment", {
-    params = "<id> <message>",
-    description = "Comment on a report",
-    privs = { redit = true },
-    func = function(name, param)
-        local id, msg = param:match("^(%d+)%s+(.+)$")
-        id = tonumber(id)
-        if not id or not msg or not reports[id] then
-            return false, "Invalid usage. /comment <id> <message>"
-        end
-        table.insert(reports[id].comments, name .. ": " .. msg)
-        return true, "Comment added to report #" .. id
-    end
+        return false, "Player not found."
+    end,
 })
