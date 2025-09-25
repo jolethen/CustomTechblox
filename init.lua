826d976e32aa4840d8d9fe297e696b8fb0ec68c1
jolethen <155651557+jolethen@users.noreply.github.com>
2025-09-25 15:01:31 +0530

    Update init.lua
    Guuide, repoo

--- init.lua
+++ init.lua
@@ -3,5 +3,5 @@ local modpath = minetest.get_modpath(minetest.get_current_modname())
 -- Load each command/system
 dofile(modpath .. "/rules.lua")
 dofile(modpath .. "/repoorts.lua")
-dofile(modpath .. "/guide.lua")
+dofile(modpath .. "/guuide.lua")
 dofile(modpath .. "/events.lua")
